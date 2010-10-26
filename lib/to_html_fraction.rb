594b5077c7fb2aaa85497d5479964dcc7ffc3e14
Torey Heinz <toreyheinz@gmail.com>
2010-10-26 14:14:33 -0400

    Round all decimals to 1/16th precision, update read me

--- lib/to_html_fraction.rb
+++ lib/to_html_fraction.rb
@@ -1,39 +1,30 @@
 class Float
   def to_html_fraction
-    fraction = case self.abs % 1.0
-    when 0.5 then '&frac12;'  # One half
-      
-    when 0.25 then '&frac14;'  # One quarter
-    when 0.75 then '&frac34;'  # Three quarters
-    
-    when 0.33...0.333333333333333333 then '&#x2153;'  # One third
-    when 0.66...0.666666666666666666 then '&#x2154;'  # Two thirds
-      
-    when 0.2 then '&#x2155;'  # One fifth
-    when 0.4 then '&#x2156;'  # Two fifths
-    when 0.6 then '&#x2157;'  # Three fifths
-    when 0.8 then '&#x2158;'  # Four fifths
-
-    when 0.16...0.166666666666666666 then '&#x2159;'  # One sixth
-    when 0.83...0.833333333333333333 then '&#x215A;'  # Five sixths
-
-    when 0.125 then '&#x215B;'  # One eighth
-    when 0.375 then '&#x215C;'  # Three eighths
-    when 0.625 then '&#x215D;'  # Five eighths
-    when 0.875 then '&#x215E;'  # Seven eighths
-      
-    when 0.18...0.1875 then ' 3/16'
-    when 0.0 then ''
-    end
-    if fraction
-      body = case self.floor
+    body = case self.floor
       when -1 then '-'
       when  0 then ''
       else self.to_i.to_s
-      end
-      body + fraction
-    else
-      self.to_s
     end
+    
+    fraction = case ((self.abs % 1.0) * 16).round.to_f / 16 #round to 1/16th precision. 
+      when 0.0625 then ' 1/16'
+      when 0.125  then '&#x215B;'  # One eighth
+      when 0.1875 then ' 3/16'
+      when 0.25   then '&frac14;'  # One quarter
+      when 0.3125 then ' 5/16'
+      when 0.375  then '&#x215C;'  # Three eighths
+      when 0.4375 then ' 7/16'
+      when 0.5    then '&frac12;'  # One half
+      when 0.5625 then ' 9/16'
+      when 0.625  then '&#x215D;'  # Five eighths
+      when 0.6875 then ' 11/16'
+      when 0.75   then '&frac34;'  # Three quarters
+      when 0.8125 then ' 13/16'
+      when 0.875  then '&#x215E;'  # Seven eighths
+      when 0.9375 then ' 15/16'
+      else ''
+    end
+    
+    body + fraction
   end
 end
--- spec/to_html_fraction_spec.rb
+++ spec/to_html_fraction_spec.rb
@@ -1,22 +1,31 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "ToHtmlFraction" do
-  it "should convert a fractional float to an html fraction" do
+  it "should convert a float to an html fraction" do
     # pending
+    0.0625.to_html_fraction.should == ' 1/16'
     0.125.to_html_fraction.should == '&#x215B;'
-    0.166.to_html_fraction.should == '&#x2159;'
-    0.2.to_html_fraction.should == '&#x2155;'
+    0.1875.to_html_fraction.should == ' 3/16'
     0.25.to_html_fraction.should == '&frac14;'
-    0.33.to_html_fraction.should == '&#x2153;'
+    0.3125.to_html_fraction.should == ' 5/16'
     0.375.to_html_fraction.should == '&#x215C;'
-    0.4.to_html_fraction.should == '&#x2156;'
+    0.4375.to_html_fraction.should == ' 7/16'
     0.5.to_html_fraction.should == '&frac12;'
-    0.6.to_html_fraction.should == '&#x2157;'
+    0.5625.to_html_fraction.should == ' 9/16'
     0.625.to_html_fraction.should == '&#x215D;'
-    0.66.to_html_fraction.should == '&#x2154;'
+    0.6875.to_html_fraction.should == ' 11/16'
     0.75.to_html_fraction.should == '&frac34;'
-    0.8.to_html_fraction.should == '&#x2158;'
-    0.833.to_html_fraction.should == '&#x215A;'
+    0.8125.to_html_fraction.should == ' 13/16'
     0.875.to_html_fraction.should == '&#x215E;'
+    0.9375.to_html_fraction.should == ' 15/16'
+
+  end
+  
+  it "should round uncommon decimals to the nearest 1/16th" do
+    0.166.to_html_fraction.should == ' 3/16'
+    0.3.to_html_fraction.should == ' 5/16'
+    0.503.to_html_fraction.should == '&frac12;'
+    0.68.to_html_fraction.should == ' 11/16'
+    0.975.to_html_fraction.should == ''
   end
 end
